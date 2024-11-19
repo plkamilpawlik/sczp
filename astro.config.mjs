677d942b965100d5035fd02fc3ec411cbe8d444a
kpawlik <kamil.pawlik@capgemini.com>
2024-11-19 22:55:32 +0100

    Added two pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [mdx(), tailwind()],
+	markdown: {
+		remarkRehype: {
+			footnoteLabel: "Źródła",
+			footnoteLabelTagName: "h4",
+		},
+	},
 });
